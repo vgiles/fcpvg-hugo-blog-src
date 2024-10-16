ec7a3225e80858e7c6c8ef4bb2c6a4ea112270da
vgiles <13183572+vgiles@users.noreply.github.com>
2024-10-16 21:03:25 +1100

    pre instance commit

--- static/projects/anim_score/mainFunctions.js
+++ static/projects/anim_score/mainFunctions.js
@@ -1,25 +1,149 @@
+"use strict";
+
+let emitter;
+let nodeColour;
+let windowTop;
+let windowRight;
+let windowLeft;
+let windowBottom;
+let fontLyric;
+let fontScoreLabel;
+let headerSize = 100;
+let footerSize = 80;
+
+
+//NB Top 70 pixels are HEADER
+
+
 function setup() {
-    createCanvas(windowWidth-20, windowHeight-20);
-
-    background(220);
-
+    scoreLayoutSetup();
     describe('A canvas');
+    nodeColour = [random(255), random(255), random(255)];
+
+    emitter = new PlayerNode(random(windowWidth-20), random(windowHeight-20), random(100)+10, nodeColour);
 }
 
-function drawEmitterCircle() {
-
+function draw() {
+    emitter.show();
 }
-
 class PlayerNode {
-    constructor(x, y, r, s, o, b) {
+    constructor(x, y, radius, colour) {
+        // x and y is position
         this.x = x;
         this.y = y;
-        this.r = r;
-        this.f = random(255);
-        this.s = s;
-        this.o = o;
-        this.history = [];
-        this.notehistory = [b];
-        this.b = b;
+        this.radius = radius;
+        this.colour = colour;
+    }
+
+    show() {
+        // construct the circle
+        stroke(20);
+        strokeWeight(2);
+        circle(this.x, this.y, this.radius);
+        fill(this.colour);
+    }
+
+    hide() {
+        stroke(0);
+        fill(0);
+    }
+
+    newLocation() {
+
     }
 }
+
+function scoreLayoutSetup() {
+    createCanvas(windowWidth-20, windowHeight-20);
+    background(200);
+
+    // Construct title
+    textFont("Times New Roman");
+    textStyle(ITALIC);
+    textSize(45);
+    textAlign(CENTER);
+    text("working title", windowWidth/2, 35);
+
+    // construct year
+    textStyle(NORMAL);
+    textSize(20);
+    textAlign(CENTER);
+    text("2024", windowWidth/2, 60);
+
+    // construct composer
+    textStyle(NORMAL);
+    textSize(20);
+    textAlign(RIGHT);
+    text("vincent giles", windowWidth-20, 45);
+
+
+    // construct score labels
+
+    textFont("Verdana");
+    textSize(12);
+    textStyle(ITALIC);
+    textAlign(LEFT);
+    text("extended register ⬆️", 20, 190);
+    text("high register ⬆️", 20, 390);
+    text("mid register ⬆️", 20, 590);
+    text("low register ⬆️", 20, 790);
+    text("subtones ⬇️", 20, 820);
+    textAlign(CENTER);
+    text("⬅️ roll/bow pos/equiv ➡️", windowWidth/2, 190);
+
+    // construct score grid lines noting that the size of the 'screen' should be at least 1040 pixels high and 820 wide. Standard laptop screen.
+
+    stroke(150);
+    line(windowWidth-windowWidth, 100, windowWidth-20, 100);
+    stroke(80);
+    strokeWeight(3);
+    line(20, 200, windowWidth-40, 200);
+    line(20, 800, windowWidth-40, 800);
+    stroke(180);
+    setLineDash([5, 5]);
+    line(windowWidth/2, 200, windowWidth/2, 800);
+    line(20, 400, windowWidth-40, 400);
+    line(20, 600, windowWidth-40, 600);
+    setLineDash([]);
+}
+
+function setLineDash(list) {
+    drawingContext.setLineDash(list);
+  }
+
+function swSketch(p) {
+    let sw = new StopWatch(3); 
+
+    p.setup = function () {
+        p.createCanvas(200, 200);
+        p.background(150);
+        sw.start();
+    };
+
+    p.draw = function () {
+        p.stopWatch();
+    };
+
+    p.stopWatch = function () {
+        textAlign(LEFT);
+        textSize(24);
+        fill(255, 0, 100);
+        var s = "";
+        if (sw.countdownFinished()) {
+            stroke(0, 127, 200);
+            s = "FINISHED.";
+        } else {
+            stroke(255);
+            s = sw.minute() + " : " + sw.second(); 
+            stroke(0);   
+        }
+        // noFill();
+        // ellipse(80, 40, 120, 80);
+        // fill(255);
+        text(s, 40, 40 + (fontSize/3));
+        // p.background(200);
+        p.background(150);
+    };
+}
+
+new p5(swSketch);
